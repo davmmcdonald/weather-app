6a0368a2df67e6867dcec445e3b0903c40232a38
David McDonald <26583259+davmmcdonald@users.noreply.github.com>
2023-04-18 14:36:47 -0500

    Set up basic weather api connection

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
-const successCallback = (position) => {
-	console.log(position);
-};
-  
-const errorCallback = (error) => {
-	console.log(error);
-};
-  
-window.navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+const temperature = document.getElementById('temperature');
+const icon = document.getElementById('icon');
+const conditions = document.getElementById('conditions');
+const region = document.getElementById('region');
+console.log(location);
+const dateTime = document.getElementById('dateTime');
+
+const searchForm = document.getElementById('searchForm');
+const searchBar = document.getElementById('searchBar');
+
+searchForm.addEventListener('submit', event => {
+	event.preventDefault();
+	updateWeather(searchBar.value);
+});
+
+function updateWeather(location) {
+	fetch(`https://api.weatherapi.com/v1/current.json?key=5f62ab4b1dfb49de9a7223916230604&q=${location}`)
+		.then(function(response) {
+			return response.json();
+		})
+		.then(function(response) {
+			console.log(response);
+			temperature.innerText = Math.round(response.current.temp_f);
+			conditions.innerText = response.current.condition.text;
+			region.innerText = `${response.location.name}, ${response.location.region}`;
+			dateTime.innerText = response.location.localtime;
+		})
+		.catch(error => {
+			console.log(error);
+		})
+}
